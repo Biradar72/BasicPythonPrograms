87441dbb6f63a017ec147c405096deeb907f4f2c
Biradar72 <basawarajbiradar72@gmail.com>
2024-12-08 20:28:52 +0530

    Update Even.py

--- Even.py
+++ Even.py
@@ -1,5 +1,6 @@
-B=int(input("Enter number2"));
-if B%2==0:
+B=int(input("Enter number2"));# Reciveing Input as  integer number  form user
+#  we have to check wether the number is divisible by 2 . If the reminder is zero then If block will print Even else The else block will print odd.
+if B%2==0: 
     print("The number is Even")
 else:
-    print("The number is Odd.")
+    print("The number is Odd.") #print statement is used to Display on the Screen
