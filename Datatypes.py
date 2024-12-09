977c0872ad4373d9b2f3862e27860f587319c7f0
Basavraj Biradar <basawarajbiradar72@gmail.com>
2024-12-09 16:23:48 +0530

    Python Basic datatypes of Numeric

---
+++ Datatypes.py
@@ -0,0 +1,13 @@
+# Python program to demonstrate numeric data types
+
+# Integer: Represents whole numbers
+a = 5
+print("The data type of variable 'a' is:\t", type(a))
+
+# Floating-point: Represents decimal numbers
+b = 5.2
+print("The data type of variable 'b' is:\t", type(b))
+
+# Complex numbers: Represents numbers with real and imaginary parts
+z = 2 + 3j
+print("The data type of variable 'z' is:\t", type(z))
